bc8d8b30d613d9df721e8d76aba359834b5022be
Raphael Nascimento <raphael.nascimento@sp.senai.br>
2022-02-19 15:22:21 -0300

    finish nextauth app

---
+++ pages/dashboard.tsx
@@ -0,0 +1,39 @@
+import { useContext, useEffect } from "react"
+import { Can } from "../components/Can"
+import { AuthContext } from "../contexts/AuthContext"
+import { setupAPIClient } from "../services/api"
+import { api } from "../services/apiClient"
+import { withSSRAuth } from "../utils/withSSRAuth"
+
+export default function Dashboard() {
+  const { user, signOut, isAuthenticated } = useContext(AuthContext)
+
+  
+
+  useEffect(() => {
+    api.get('/me')
+    .then(response => console.log(response))
+  }, [])
+
+  return (
+    <>
+      <h1>Dahsboard: {user?.email}</h1>
+
+      <button onClick={signOut}>Sign out</button>
+
+      <Can permissions={['metrics.list']}>
+        <div>Métricas</div>
+      </Can>
+    </>
+  )
+}
+
+export const getServerSideProps = withSSRAuth(async (ctx) => {
+  const apiClient = setupAPIClient(ctx)
+  const response = await apiClient.get('/me')
+  console.log(response);
+
+  return {
+    props: {}
+  }
+})
